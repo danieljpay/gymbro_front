12ba8c36ff9bf3eae2238731329f041b91084528
danieljpay <danielj.pay@hotmail.com>
2024-04-04 20:58:15 -0600

    main models added

---
+++ src/app/models/client.model.ts
@@ -0,0 +1,10 @@
+import { DeclarationListEmitMode } from "@angular/compiler";
+
+export interface Client {
+    id: number;
+    address: string;
+    email: string;
+    last_name: string;
+    name: string;
+    phone: number;
+}
---
+++ src/app/models/membership.model.ts
@@ -0,0 +1,7 @@
+export interface Membership {
+    id: number,
+    cost: number;
+    description: string;
+    duration: string;
+    name: string;
+}
