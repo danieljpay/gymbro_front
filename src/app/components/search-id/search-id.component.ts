49fd953eaa44d887da389e9d78c47693b421b1a3
danieljpay <danielj.pay@hotmail.com>
2024-04-04 20:57:54 -0600

    Search-id component added

---
+++ src/app/components/search-id/search-id.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-id',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-id.component.html',
+  styleUrl: './search-id.component.scss'
+})
+export class SearchIdComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchIdComponent } from '../../components/search-id/search-id.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SearchIdComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
