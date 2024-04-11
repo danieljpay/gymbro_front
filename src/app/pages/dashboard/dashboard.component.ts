04706d325a8d0f221a93fca154d50a45a6849e7e
danieljpay <danielj.pay@hotmail.com>
2024-04-10 21:07:58 -0600

    Some cards added to Dashboard component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { ClientsService } from '../../services/clients.service';
+import { MembershipsService } from '../../services/memberships.service';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -8,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  totalClients: number = 0;
+  totalMemberships: number = 0;
 
+  constructor(private clientsService: ClientsService, private membershipsService: MembershipsService) {}
+
+  ngOnInit(): void {
+    this.loadData(this.clientsService, this.membershipsService);
+  }
+
+  loadData(clientsService: ClientsService, membershipsService: MembershipsService) {
+    let totalClients = clientsService.getClients().length;
+    let totalMemberships = membershipsService.getMemberships().length;
+
+    this.totalClients = totalClients;
+    this.totalMemberships = totalMemberships;
+  }
 }
